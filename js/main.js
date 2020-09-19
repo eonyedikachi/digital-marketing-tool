8cf020df11f5f9934b440cb7ef0205758bbbd9dd
Osaiteh <26644393+Osaiteh@users.noreply.github.com>
2020-09-19 12:02:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,18 @@ hamburger.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+// TAB
+function openSetting(evt, settingName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(settingName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
