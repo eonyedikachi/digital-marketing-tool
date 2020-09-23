ab7d4912f1f78378b48bc7d201a488f6d7cf2db5
Ekeso <ekesolonge@gmail.com>
2020-09-23 16:42:54 +0100

    added eye toggle to password on register.html

--- js/main.js
+++ js/main.js
@@ -28,3 +28,16 @@ function openSetting(evt, settingName) {
   document.getElementById(settingName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Toggle Password
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  // toggle the eye slash icon
+  this.classList.toggle("fa-eye-slash");
+});
