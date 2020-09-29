003a8cc58356dff9db0019ee9ad08e5a5a84aae3
Malik <malikmukhtar63@gmail.com>
2020-09-29 21:35:54 +0100

    user registration

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ hamburger.addEventListener("click", () => {
   }
 });
 
-// TAB
+// Settings Tab
 function openSetting(evt, settingName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -41,3 +41,35 @@ togglePassword.addEventListener("click", function (e) {
   // toggle the eye slash icon
   this.classList.toggle("fa-eye-slash");
 });
+
+//Creating Local Storage
+let database = JSON.parse(localStorage.getItem("database"));
+
+if (database == null) {
+  database = [];
+}
+// console.log(database);
+//Registering User
+
+const registerUser = () => {
+  newUser = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    username: document.getElementById("username").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+  //Storing inside the array
+  database.push(newUser);
+
+  //Routing to dashboard
+  location.assign("../dashboard.html");
+
+  //updating local storage
+  localStorage.setItem("database", JSON.stringify(database));
+};
+
+let registerButton = document.getElementById("register");
+registerButton.addEventListener("click", registerUser);
+
+console.log(database);
