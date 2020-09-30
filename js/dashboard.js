64b16d350aa6c7bb6b2d32706597f2624691fc1c
Ekeso <ekesolonge@gmail.com>
2020-09-30 16:59:27 +0100

    edited dashboard and signin.js to store current user

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,18 @@
 let database = JSON.parse(localStorage.getItem("database"));
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 if (database == null) {
   database = [];
 }
 
-let randomNum = Math.floor(Math.random() * database.length);
+if (currentUser == null) {
+  currentUser = [];
+}
 
+// fetches most recent logged in user
+i = currentUser.length - 1;
+
+// Displays Users Name
 document.getElementById(
   "usersname111"
-).innerText = `${database[randomNum].firstName} ${database[randomNum].lastName}`;
+).innerText = `${currentUser[i].firstName} ${currentUser[i].lastName}`;
--- js/sigin.js
+++ js/sigin.js
@@ -1,7 +1,12 @@
 let database = JSON.parse(localStorage.getItem("database"));
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 if (database == null) {
   database = [];
+}
+
+if (currentUser == null) {
+  currentUser = [];
 }
 //Logging User
 const logInUser = (e) => {
@@ -9,9 +14,17 @@ const logInUser = (e) => {
   let username = document.getElementById("login-username").value;
   let password = document.getElementById("login-password").value;
 
-  database.find(
+  // Finds username and password in database
+  let user = database.find(
     (element) => element.username == username && element.password == password
-  )
+  );
+
+  // Adds current user login to storage
+  currentUser.push(user);
+  localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+  // User validation
+  user
     ? window.location.assign("../dashboard.html")
     : alert("Username or Password not correct");
 };
