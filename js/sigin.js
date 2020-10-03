ea206bd78518ad75c2973e1a9d7bd0a985704c3d
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2020-10-03 10:00:40 +0100

    Sign in Validation

--- js/sigin.js
+++ js/sigin.js
@@ -9,25 +9,77 @@ if (currentUser == null) {
   currentUser = [];
 }
 //Logging User
-const logInUser = (e) => {
-  e.preventDefault();
-  let username = document.getElementById("login-username").value;
-  let password = document.getElementById("login-password").value;
+// const logInUser = (e) => {
+//   e.preventDefault();
+//   let username = document.getElementById("login-username").value;
+//   let password = document.getElementById("login-password").value;
 
-  // Finds username and password in database
-  let user = database.find(
-    (element) => element.username == username && element.password == password
-  );
+//   // Finds username and password in database
+//   let user = database.find(
+//     (element) => element.username == username && element.password == password
+//   );
 
-  // Adds current user login to storage
-  currentUser.push(user);
-  localStorage.setItem("currentUser", JSON.stringify(currentUser));
+//   // Adds current user login to storage
+//   currentUser.push(user);
+//   localStorage.setItem("currentUser", JSON.stringify(currentUser));
 
-  // User validation
-  user
-    ? window.location.assign("../dashboard.html")
-    : alert("Username or Password not correct");
-};
+//   // User validation
+//   user
+//     ? window.location.assign("../dashboard.html")
+//     : alert("Username or Password not correct");
+// };
 
 //Targeting the login  button
-document.getElementById("login-button").addEventListener("click", logInUser);
+// document.getElementById("Login").addEventListener("click", logInUser);
+
+
+
+
+
+
+var use = document.querySelector('#use');
+use.addEventListener('keyup', function(){
+  var u_times = document.querySelector('.u_times');
+  var u_check = document.querySelector('.u_check');
+  if(use.value.length == 0 || use .value.length < 2){
+    use.style.border = '1px solid red';
+    u_times.style.display = 'block';
+    u_check.style.display = 'none';
+} else{
+  use.style.border = '1px solid green';
+  u_times.style.display = 'none';
+  u_check.style.display = 'block';
+}
+})
+
+
+
+var pass = document.querySelector('#pass');
+pass.addEventListener('keyup', function(){
+  var p_times = document.querySelector('.p_times');
+  var p_check = document.querySelector('.p_check');
+  if(pass.value.length == 0 || pass .value.length < 8){
+    pass.style.border = '1px solid red';
+    p_times.style.display = 'block';
+    p_check.style.display = 'none';
+} else{
+  pass.style.border = '1px solid green';
+  p_times.style.display = 'none';
+  p_check.style.display = 'block'; 
+}
+})
+
+function validate(){
+  if(use.value == 0 || use.value.length < 2){
+    document.getElementById('error').innerHTML = 'Please fill the required fields!';  
+    return false;
+
+  }else if(pass.value == 0 || pass.value.length < 8){
+    document.getElementById('error').innerHTML = 'Please fill the required fields!';  
+    return false;
+
+  }else{
+    alert('Successfully logged in');
+  }
+}
+
