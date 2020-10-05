7c2fa58021836bca981e2f031b22f0aaa0874a8c
Ekeso <ekesolonge@gmail.com>
2020-10-05 16:36:33 +0100

    fixed dashboard to fetch names correctly

--- js/setting.js
+++ js/setting.js
@@ -1,45 +1,44 @@
-
-
-
 // TAB
 function openSetting(evt, settingName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(settingName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(settingName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
+// fetches most recent logged in user
+i = currentUser.length - 1;
 //FirstName Display
-var mFirstName = database[i].firstName;
+var mFirstName = currentUser[i].firstName;
 var uFirstName = document.querySelectorAll(".first-name");
-for (var i = 0; i < uFirstName.length; i++) {
+for (let i = 0; i < uFirstName.length; i++) {
   uFirstName[i].innerHTML = mFirstName;
 }
 
 //LastName Display
-var mLastName = database[i].lastName;
+var mLastName = currentUser[i].lastName;
 var uLastName = document.querySelectorAll(".last-name");
-for (var i = 0; i < uLastName.length; i++) {
+for (let i = 0; i < uLastName.length; i++) {
   uLastName[i].innerHTML = mLastName;
 }
 
 //UserName Display
-var mUserName = database[i].userName;
+var mUserName = currentUser[i].userName;
 var uUserName = document.querySelectorAll(".user-name");
-for (var i = 0; i < uUserName.length; i++) {
+for (let i = 0; i < uUserName.length; i++) {
   uUserName[i].innerHTML = mUserName;
 }
 
 //Email Display
-var mEmail = database[i].email;
+var mEmail = currentUser[i].email;
 var uEmail = document.querySelectorAll(".email-address");
-for (var i = 0; i < uEmail.length; i++) {
+for (let i = 0; i < uEmail.length; i++) {
   uEmail[i].innerHTML = mEmail;
 }
