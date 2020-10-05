bcdf4bd9823f35d836f514f5ce3cd697799bba9f
Osaiteh <26644393+Osaiteh@users.noreply.github.com>
2020-10-05 02:55:13 +0100

    Update setting.html

--- js/setting.js
+++ js/setting.js
@@ -1,3 +1,4 @@
+
 var randomNumber = Math.floor(Math.random() * 9) + 1;
 var randomAvatar = "avatar" + randomNumber + ".png";
 var randomImageSource = "images/" + randomAvatar;
@@ -18,3 +19,31 @@ function openSetting(evt, settingName) {
     document.getElementById(settingName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+//FirstName Display
+var mFirstName = database[i].firstName;
+var uFirstName = document.querySelectorAll("p.first-name");
+for (var i = 0; i < uFirstName.length; i++) {
+  uFirstName[i].innerHTML = mFirstName;
+}
+
+//LastName Display
+var mLastName = database[i].lastName;
+var uLastName = document.querySelectorAll("p.last-name");
+for (var i = 0; i < uLastName.length; i++) {
+  uLastName[i].innerHTML = mLastName;
+}
+
+//UserName Display
+var mUserName = database[i].userName;
+var uUserName = document.querySelectorAll("p.user-name");
+for (var i = 0; i < uUserName.length; i++) {
+  uUserName[i].innerHTML = mUserName;
+}
+
+//Email Display
+var mEmail = database[i].email;
+var uEmail = document.querySelectorAll("p.email-address");
+for (var i = 0; i < uEmail.length; i++) {
+  uEmail[i].innerHTML = mEmail;
+}
