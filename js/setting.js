c002f535b1d08879a0cc13144411d26206691b1e
Osaiteh <26644393+Osaiteh@users.noreply.github.com>
2020-10-06 06:31:29 +0100

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -15,14 +15,14 @@ function openSetting(evt, settingName) {
 
 // fetches most recent logged in user
 i = currentUser.length - 1;
-//FirstName Display
+//First Name Display
 var mFirstName = currentUser[i].firstName;
 var uFirstName = document.querySelectorAll(".first-name");
 for (let i = 0; i < uFirstName.length; i++) {
   uFirstName[i].innerHTML = mFirstName;
 }
 
-//LastName Display
+//Last Name Display
 var mLastName = currentUser[i].lastName;
 var uLastName = document.querySelectorAll(".last-name");
 for (let i = 0; i < uLastName.length; i++) {
@@ -36,9 +36,51 @@ for (let i = 0; i < uUserName.length; i++) {
   uUserName[i].innerHTML = mUserName;
 }
 
+//Website Display
+var mWebsite = currentUser[i].website;
+var uWebsite = document.querySelectorAll(".website");
+for (let i = 0; i < uWebsite.length; i++) {
+  uWebsite[i].innerHTML = mWebsite;
+}
+
 //Email Display
 var mEmail = currentUser[i].email;
 var uEmail = document.querySelectorAll(".email-address");
 for (let i = 0; i < uEmail.length; i++) {
   uEmail[i].innerHTML = mEmail;
+}
+
+//Contact Name Display
+var mContactName = currentUser[i].contactName;
+var uContactName = document.querySelectorAll(".contact-name");
+for (let i = 0; i < uContactName.length; i++) {
+  uContactName[i].innerHTML = mContactName;
+}
+
+//Company Name Display
+var mCompanyName = currentUser[i].companyName;
+var uCompanyName = document.querySelectorAll(".company-name");
+for (let i = 0; i < uCompanyName.length; i++) {
+  uCompanyName[i].innerHTML = mCompanyName;
+}
+
+//Billing Address Display
+var BillingAddress = currentUser[i].billingAddress;
+var uBillingAddress = document.querySelectorAll(".billing-address");
+for (let i = 0; i < uBillingAddress.length; i++) {
+  uBillingAddress[i].innerHTML = mBillingAddress;
+}
+
+//Billing Phone Number Display
+var mBillingPhoneNumber = currentUser[i].billingPhoneNumber;
+var uBillingPhoneNumber = document.querySelectorAll(".billing-phone-number");
+for (let i = 0; i < uBillingPhoneNumber.length; i++) {
+  uBillingPhoneNumber[i].innerHTML = mBillingPhoneNumber;
+}
+
+//Billing Email Address Display
+var mBillingEmailAddress = currentUser[i].billingEmailAddress;
+var uBillingEmailAddress = document.querySelectorAll(".billing-email-address");
+for (let i = 0; i < uBillingEmailAddress.length; i++) {
+  uBillingEmailAddress[i].innerHTML = mBillingEmailAddress;
 }
