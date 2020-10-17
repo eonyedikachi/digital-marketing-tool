da0dfe278e7ffeef6690938a0f1db00ddaa550a6
Aminu Muhammad <aminumuhammad6035@gmail.com>
2020-10-17 22:31:32 +0100

    whizick added to suggestion page

---
+++ js/Feedback.js
@@ -0,0 +1,82 @@
+// Setting bold property
+
+
+function boldd() {
+const p=   document.getElementById("comment");
+ if(p.style.fontWeight!=="bold"){
+ p.style.fontWeight = "bold"
+}else{
+p.style.fontWeight = "normal"
+}
+}
+
+// setting ittalize
+function ittalize() {
+  const  p=   document.getElementById("comment");
+    if(p.style.fontStyle!=="italic"){
+    p.style.fontStyle = "italic"
+   }else{
+   p.style.fontStyle = "normal"
+   }
+   }
+
+// underline
+function underline() {
+    const  p=   document.getElementById("comment");
+      if(p.style.textDecoration!=="underline"){
+      p.style.textDecoration = "underline"
+     }else{
+     p.style.textDecoration = "none"
+     }
+     }
+
+    //  text-align-right
+    
+    function tright() {
+        const  p=   document.getElementById("comment");
+          if(p.style.textAlign!=="right"){
+          p.style.textAlign = "right";
+         }
+        }
+    //  text-align-left
+    
+    function tleft() {
+        const  p=   document.getElementById("comment");
+          if(p.style.textAlign!=="left"){
+          p.style.textAlign = "left";
+         }
+        }
+ //  text-align-center
+    
+ function tcenter() {
+    const  p=   document.getElementById("comment");
+      if(p.style.textAlign!=="center"){
+      p.style.textAlign = "center";
+     }
+    }
+// font-size
+
+function fontsize(){
+const  p=   document.getElementById("comment");
+const size = document.getElementById("fontsize").value;
+p.style.fontSize= `${size}px`
+}
+
+// collecting suggestion content
+
+// unbold= text.style.cssText="Font-weight:100";
+//     if(text.style.cssText !=unbold){
+//         text.style.cssText=bold
+//     }
+//            else if( text.style.cssText=bold){
+//             text.style.cssText=unbold
+//            }
+//         }
+// setting Itallic
+// function itallic(){
+//     itallicc= text.style.cssText="font-style: italic;"
+//     initallicc= text.style.cssText="font-style: normal";
+//     if( text.style.cssText != itallicc){
+//         text.style.cssText=itallicc
+//     }
+// }
