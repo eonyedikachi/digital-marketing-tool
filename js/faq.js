be03d589f9b9186a8b4b968204c2e9d95769ec5c
Aminu Muhammad <aminumuhammad6035@gmail.com>
2020-10-25 18:37:35 +0100

    redesign FAQ

---
+++ js/faq.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+  $("#navia").click(function(){
+    $("#navia .fa-greater-than").toggleClass("rtoate180");
+    $("#navigationa").stop().slideToggle(1000);
+  });
+});
+$(document).ready(function(){
+  $("#navib").click(function(){
+    $("#navib .fa-greater-than").toggleClass("rtoate180");
+    $("#navigationb").stop().slideToggle(1000);
+  });
+});
+$(document).ready(function(){
+  $("#navic").click(function(){
+    $("#navic .fa-greater-than").toggleClass("rtoate180");
+    $("#navigationc").stop().slideToggle(1000);
+  });
+});
+$(document).ready(function(){
+  $("#navid").click(function(){
+    $("#navid .fa-greater-than").toggleClass("rtoate180");
+    $("#navigationd").stop().slideToggle(1000);
+  });
+});
+ 
+//   
