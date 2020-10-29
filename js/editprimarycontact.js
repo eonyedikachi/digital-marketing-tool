e82bbb94ec25a275d27e220982d64e90f3fff5a4
Osaiteh <26644393+Osaiteh@users.noreply.github.com>
2020-10-29 09:19:58 +0100

    Create editprimarycontact.js

---
+++ js/editprimarycontact.js
@@ -0,0 +1,64 @@
+// fetches most recent logged in user
+i = currentUser.length - 1;
+// let database = JSON.parse(localStorage.getItem("database"));
+
+var source;
+
+
+
+editUser = currentUser[i];
+document.getElementById("contactContactName").value = editUser.firstName;
+document.getElementById("lastName").value = editUser.lastName;
+document.getElementById("userName").value = editUser.userName;
+document.getElementById("emailAddress").value = editUser.email;
+document.getElementById("website").value = editUser.website;
+
+if (
+  document.getElementById("website").value == "" ||
+  document.getElementById("website").value == null ||
+  document.getElementById("website").value == undefined
+) {
+  document.getElementById("website").value = "";
+}
+var randomNumber = Math.floor(Math.random() * 9) + 1;
+var randomAvatar = "avatar" + randomNumber + ".png";
+var randomImageSource = "images/" + randomAvatar;
+if (editUser.pics == null || editUser.pics == undefined) {
+  document.querySelector(".profile img").setAttribute("src", randomImageSource);
+} else {
+  document.querySelector(".profile img").setAttribute("src", editUser.pics);
+}
+
+function cUpdate() {
+  let edited = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    userName: document.getElementById("userName").value,
+    email: document.getElementById("emailAddress").value,
+    password: editUser.password,
+    role: editUser.role,
+    pics: editUser.pics,
+    website: document.getElementById("website").value,
+  };
+  currentUser[i] = edited;
+  localStorage.setItem("database", JSON.stringify(currentUser));
+  localStorage.setItem("currentUser", JSON.stringify(currentUser));
+  // display();
+  
+  window.location.assign("../setting.html")
+}
+
+//upload picture or video from device
+var loadFile = function (e) {
+  // Upload image
+  var image = document.getElementById("output");
+
+  var reader = new FileReader();
+  reader.onload = function () {
+    editUser.pics = reader.result;
+    image.src = editUser.pics;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+
+  localStorage.setItem("currentUser", JSON.stringify(currentUser));
+};
