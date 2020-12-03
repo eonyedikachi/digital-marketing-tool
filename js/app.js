ec3af0ec987256935b680b75ff641cb3a470512e
Malik <malikmukhtar63@gmail.com>
2020-12-04 00:18:19 +0100

    created suggestion controller

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-const express = require("express"); // express module
-const bodyParser = require("body-parser"); // body-parser middleware
-const emailTemplate = require("../controllers/emailTemplateController"); // emailTemplateController module
+const express = require('express'); // express module
+const bodyParser = require('body-parser'); // body-parser middleware
+const emailTemplate = require('../controllers/emailTemplateController'); // emailTemplateController module
+const suggestions = require('../controllers/suggestions'); //Suggestions Controller
 
 // Dynamic port listener
 const port = process.env.PORT || 3000; // set port
@@ -8,6 +9,7 @@ const app = express(); // express init
 app.use(bodyParser.json()); // Middleware use with express
 
 emailTemplate(app);
+suggestions(app);
 
 // Listen on port
 app.listen(port, () => console.log(`Listening on port ${port}...`));
