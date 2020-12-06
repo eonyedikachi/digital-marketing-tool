3a95d1cbeb1035c42bc49fa06fa19e76b1bfea21
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2020-12-06 11:29:08 +0100

    userController.js craeted

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const emailTemplate = require('../controllers/emailTemplateController'); // emai
 const suggestions = require('../controllers/suggestions'); //Suggestions Controller
 const reply = require('../controllers/reply'); //Reply Controller
 const billing_info = require('../controllers/billing_info'); //Reply Controller
+const users = require('../controllers/userControllers') //userController module
+
 
 // Dynamic port listener
 const port = process.env.PORT || 3000; // set port
@@ -13,6 +15,7 @@ app.use(bodyParser.json()); // Middleware use with express
 emailTemplate(app);
 suggestions(app);
 reply(app);
+users(app);
 billing_info(app);
 
 // Listen on port
